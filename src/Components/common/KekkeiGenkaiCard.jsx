7631899a1c4eb34e2eba37364c7a426453fa5d27
rc5292684 <rc5292684@gmail.com>
2023-12-26 16:30:22 +0530

    fixed clan card , kekkeigankai card , tailed beast card

--- src/Components/common/KekkeiGenkaiCard.jsx
+++ src/Components/common/KekkeiGenkaiCard.jsx
@@ -9,9 +9,7 @@ const KekkeiGenkaiCard = ({ data }) => {
             sx={{
                 backgroundImage: 'linear-gradient(285.53deg, rgba(46, 46, 255, 0.6) -27.34%, rgba(253, 118, 48, 0.6) 112.3%)',
                 borderRadius: '7px',
-                width: {
-                    md: '325px'
-                },
+                width: '325px',
                 padding: 2,
                 position: 'relative',
                 cursor: 'pointer'
