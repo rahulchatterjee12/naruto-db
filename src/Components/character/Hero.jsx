b6adeab06ccd29308ccc3a655b27006c02381940
rc5292684 <rc5292684@gmail.com>
2023-12-29 11:46:56 +0530

    style : update photo position for mobile view style : update card width for mobile view

--- src/Components/character/Hero.jsx
+++ src/Components/character/Hero.jsx
@@ -12,15 +12,15 @@ const Hero = () => {
         <>
             {!isLoading ?
                 <div
-                    className='h-[60vh] md:mt-[10%]'
+                    className='h-[60vh] mt-5 md:mt-[10%]'
                     style={{
                         background: 'radial-gradient(136.26% 206.75% at 50% 50%, rgba(255, 255, 255, 0.8) 0%, rgba(255, 255, 255, 0.8) 0%, rgba(233, 131, 82, 0.8) 0.01%, rgba(233, 132, 84, 0.791746) 0.02%, rgba(244, 110, 46, 0.783655) 1.04%, rgba(255, 255, 255, 0) 100%)',
                         borderRadius: '10px',
                         position: 'relative'
                     }}
                 >
-                    <img src={data?.images[0] ? data?.images[0] : ''} className='md:w-[20vw]  md:h-[260px] rounded absolute -top-16 md:right-16 lg:right-36 bg-white object-cover' />
-                    <div className='w-[400px] font-serif italic p-6 text-black gap-6'>
+                    <img src={data?.images[0] ? data?.images[0] : ''} className='w-[25vw] mt-2 md:w-[20vw]   md:h-[260px] rounded absolute top-0 -md:top-16 right-2 md:right-16 lg:right-36 bg-white object-cover' />
+                    <div className='md:w-[400px] font-serif italic p-6 text-black gap-6'>
                         <h2 className='text-3xl'>{data.name}</h2>
                         <h2 className='text-lg '>Clan : {data?.personal?.clan ? data?.personal?.clan : '-'}</h2>
                         <h2 className='text-lg'>Birthday: {data.personal.birthdate ? data.personal.birthdate : '-'}</h2>
