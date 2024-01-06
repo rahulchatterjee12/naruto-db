a3585f1214876d1bbc728e286d8c88de0abc727d
rc5292684 <rc5292684@gmail.com>
2024-01-06 15:13:52 +0530

    style : change all cards background and border

--- src/Components/common/TailedBeastCard.jsx
+++ src/Components/common/TailedBeastCard.jsx
@@ -4,7 +4,9 @@ const TailedBeastCard = ({ data }) => {
     return (
         <Box
             sx={{
-                backgroundImage: 'linear-gradient(285.53deg, rgba(46, 46, 255, 0.6) -27.34%, rgba(253, 118, 48, 0.6) 112.3%)',
+                backgroundImage: 'rgba(74, 40, 242, 0.05)',
+                backdropFilter: 'blur(5px)',
+                boxShadow: '0 4px 6px rgba(159, 52, 255, 0.2), inset 0 0 5px rgba(159, 52, 255, 0.2)',
                 borderRadius: '7px',
                 width: '325px',
                 padding: 2,
