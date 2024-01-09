499e6f1d70c0b249b4c56d9dfe92e71a4fa62d38
rc5292684 <rc5292684@gmail.com>
2024-01-09 12:32:22 +0530

    style : add animation to team card

--- src/Components/common/TeamCard.jsx
+++ src/Components/common/TeamCard.jsx
@@ -1,8 +1,8 @@
+import React from 'react';
 import { useGetCharacterByIdQuery } from '@/features/apiSlice';
 import { Box } from '@mui/material';
-import { useRouter } from 'next/navigation'
-import React from 'react'
-
+import { useRouter } from 'next/navigation';
+import { motion } from 'framer-motion';
 
 const TeamMember = ({ id }) => {
     const { data } = useGetCharacterByIdQuery({ id: id })
@@ -28,25 +28,36 @@ const TeamMember = ({ id }) => {
 const TeamCard = ({ data }) => {
     const router = useRouter();
     return (
-        <Box
-            sx={{
-                background: 'rgba(74, 40, 242, 0.05)',
-                backdropFilter: 'blur(5px)',
-                borderRadius: '7px',
-                border: '1px solid rgba(21,32,54,.4)',
-                width: '280px',
-                padding: 2,
-                position: 'relative',
-                cursor: 'pointer',
-                boxShadow: '0 4px 6px rgba(159, 52, 255, 0.2), inset 0 0 10px rgba(159, 52, 255, 0.2)'
-            }}
+        <motion.div
+            initial={{ opacity: 0, y: -200 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
             onClick={() => { router.push(`/teams/${data.id}`) }}
         >
-            <h3 className='text-center font-bold  font-mono '>{data.name}({data.characters.length})</h3>
-            <div className='flex gap-4 mt-4 font-serif'>
-                {data.characters.slice(0, 3).map(id => <TeamMember id={id} key={id} />)}
-            </div>
-        </Box>
+            <Box
+                sx={{
+                    background: 'rgba(74, 40, 242, 0.05)',
+                    backdropFilter: 'blur(5px)',
+                    borderRadius: '7px',
+                    border: '1px solid rgba(21,32,54,.4)',
+                    width: '280px',
+                    height: '225px',
+                    padding: 2,
+                    position: 'relative',
+                    cursor: 'pointer',
+                    boxShadow: '0 4px 6px rgba(159, 52, 255, 0.2), inset 0 0 10px rgba(159, 52, 255, 0.2)',
+                    overflow: 'hidden'
+                }}
+            >
+                <h3 className='text-center font-bold  font-mono '>{data.name}({data.characters.length})</h3>
+                <div className='flex gap-4 mt-4 font-serif'>
+                    {data.characters.slice(0, 3).map(id => <TeamMember id={id} key={id} />)}
+                </div>
+            </Box>
+        </motion.div>
     )
 }
 
