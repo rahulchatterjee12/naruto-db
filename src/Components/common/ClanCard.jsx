a8da2ab1733bf6950fbffe8cc2c013557116fd87
rc5292684 <rc5292684@gmail.com>
2024-01-09 12:35:53 +0530

    style : add animation to clan card

--- src/Components/common/ClanCard.jsx
+++ src/Components/common/ClanCard.jsx
@@ -1,26 +1,36 @@
+import React from 'react';
 import { Box } from '@mui/material';
 import { useRouter } from 'next/navigation';
-import React from 'react'
+import { motion } from 'framer-motion';
 
 const ClanCard = ({ data }) => {
     const router = useRouter()
     return (
-        <Box
-            sx={{
-                backgroundImage: 'rgba(74, 40, 242, 0.05)',
-                backdropFilter: 'blur(5px)',
-                boxShadow: '0 4px 6px rgba(159, 52, 255, 0.2), inset 0 0 5px rgba(159, 52, 255, 0.2)',
-                borderRadius: '7px',
-                width: '325px',
-                padding: 2,
-                position: 'relative',
-                cursor: 'pointer'
-            }}
+        <motion.div
+            initial={{ opacity: 0, y: -200 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
             onClick={() => { router.push(`/clans/${data.id}`) }}
         >
-            <h2>Name : {data.name} </h2>
-            <h2>Clan Members : {data.characters.length} </h2>
-        </Box>
+            <Box
+                sx={{
+                    backgroundImage: 'rgba(74, 40, 242, 0.05)',
+                    backdropFilter: 'blur(5px)',
+                    boxShadow: '0 4px 6px rgba(159, 52, 255, 0.2), inset 0 0 5px rgba(159, 52, 255, 0.2)',
+                    borderRadius: '7px',
+                    width: '325px',
+                    padding: 2,
+                    position: 'relative',
+                    cursor: 'pointer'
+                }}
+            >
+                <h2>Name : {data.name} </h2>
+                <h2>Clan Members : {data.characters.length} </h2>
+            </Box>
+        </motion.div>
     )
 }
 
