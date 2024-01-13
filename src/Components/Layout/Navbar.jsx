a2b1578a6ec34b6bc0624b1c0590e0919152c658
rc5292684 <rc5292684@gmail.com>
2024-01-13 15:27:35 +0530

    style : animation in navbar

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -1,151 +1,171 @@
 "use client";
 
-import { Menu, MenuItem } from '@mui/material'
-import Link from 'next/link'
+import Link from 'next/link';
 import React, { useState } from 'react'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import { useRouter } from 'next/navigation';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion, } from 'framer-motion'
+
+const navLinks = [
+    {
+        title: 'Akatuski',
+        link: '/akatsuki',
+    },
+    {
+        title: 'Characters',
+        link: '/characters',
+    },
+    {
+        title: 'Clans',
+        link: '/clans',
+    },
+    {
+        title: 'Kekkei Genkai',
+        link: '/kekkei-genkai',
+    },
+    {
+        title: 'Tailed Beasts',
+        link: '/tailed-beasts',
+    },
+    {
+        title: 'Teams',
+        link: '/teams',
+    },
+    {
+        title: 'Villages',
+        link: '/villages',
+    },
+
+]
+
+
+const NavLink = ({ title, href }) => {
+    const linkVars = {
+        initial: {
+            y: "30vh",
+            transition: {
+                duration: .5
+            }
+        },
+        open: {
+            y: 0,
+            transition: {
+                duration: .75
+            }
+        }
+    }
+    return (
+        <div className='overflow-hidden'>
+            <motion.div
+                variants={linkVars}
+                className='text-4xl uppercase text-white'>
+                <Link href={href} style={{
+                    fontFamily: 'serif',
+                    textShadow: 'white 1px 0 10px',
+                }}>{title}</Link>
+            </motion.div>
+        </div>
+    )
+}
+
 
 const Navbar = () => {
-    const router = useRouter();
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+    const [open, setOpen] = useState(false);
+    const toggleMenu = () => {
+        setOpen(!open)
+    }
+
+    const menuVers = {
+        initial: {
+            scaleY: 0
+        },
+        animate: {
+            scaleY: 1,
+            transition: {
+                duration: 0.5,
+                ease: [0.12, 0, 0.39, 0]
+            }
+        },
+        exit: {
+            scaleY: 0,
+            transition: {
+                delay: 0.5,
+                duration: 0.5,
+                ease: [0.12, 0, 0.39, 1]
+            }
+        }
+    }
+
+    const containerVars = {
+        initial: {
+            transition: {
+                staggerChildren: 0.09,
+                staggerDirection: -1,
+            }
+        },
+        open: {
+            transition: {
+                staggerChildren: 0.09,
+                delayChildren: 0.3,
+                staggerDirection: 1,
+            }
+        }
+    }
+
     return (
-        <motion.nav
-            variants={{
-                hidden: { opacity: 0, x: 0, y: -100 },
-                enter: { opacity: 1, x: 0, y: 0 },
-            }}
-            initial="hidden"
-            animate="enter"
-            transition={{ type: "linear" }}
-            className='h-16  bg-[#FFFFFF] bg-opacity-[16%] blur-50 -mx-[5vw] md:-mx-[12vw] lg:-mx-[15vw] '>
-            <div className='mx-[12vw] flex justify-between'>
-                <Link href='/' className='flex'>
-                    <img className='pt-2' src="/logo.svg" alt="Logo" width={50} />
-                    <h2 className='mt-[13px] font-bold font-serif text-2xl bg-gradient-to-tl from-[#ea00ff] to-[#00d0ff] text-transparent bg-clip-text'>NARUTO DB</h2>
-                </Link>
-                <div className='flex gap-3 mt-[1.25rem]'>
-                    <div onClick={handleClick} className='h-fit p-1 hover:text-orange-400 cursor-pointer'>
-                        View <span>{!open ? <KeyboardArrowDownIcon /> : <KeyboardArrowUpIcon />}</span>
+        <header>
+            <nav>
+                <div className='flex justify-between items-center py-8 lg:py-4 px-2'>
+                    <div className='w-5 h-5 bg-orange-400 rounded-full'>
+                        <span className='text-sm font-semibold tracking-widest'>NarutoDB</span>
                     </div>
-                    <Menu
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={handleClose}
-                        variant='menu'
-                        sx={{
-                            '.MuiMenu-paper': {
-                                backgroundColor: 'rgba(0,0,0,0)',
-                                border: '1px solid rgba(234,0 ,225,0.2)'
-                            }
+                    <div
+                        className='cursor-pointer text-md text-white'
+                        onClick={toggleMenu}
+                    >
+                        Menu
+                    </div>
+                </div>
+            </nav>
+            <AnimatePresence>
+                {open &&
+                    <motion.div
+                        variants={menuVers}
+                        initial='initial'
+                        animate='animate'
+                        exit='exit'
+                        className='fixed left-0 top-0 w-full h-screen p-10 origin-top bg-white text-black z-10'
+                        style={{
+                            backgroundImage: 'url("/nav.jpg")',
+                            backgroundPosition: 'center',
+                            backgroundSize: 'cover'
                         }}
                     >
-                        <MenuItem
-                            sx={{
-                                background: 'rgba(232, 107, 39, 0)',
-                                marginTop: -1,
-                                color: 'black',
-                                ":hover": {
-                                    background: 'rgba(232, 107, 39, 0)',
-                                    color: 'blue'
+                        <div className='flex h-full flex-col'>
+                            <div className='flex md:mx-24 justify-between text-white'>
+                                <div className='w-5 h-5 bg-orange-400 rounded-full'>
+                                    <span className='text-sm font-semibold tracking-widest'>NarutoDB</span>
+                                </div>
+                                <p
+                                    className='cursor-pointer'
+                                    onClick={toggleMenu}
+                                >Close</p>
+                            </div>
+                            <motion.div
+                                variants={containerVars}
+                                initial='initial'
+                                animate='open'
+                                exit='initial'
+                                className='flex flex-col h-full justify-center items-center gap-4'>
+                                {
+                                    navLinks.map((link, index) => (
+                                        <NavLink key={index} title={link.title} href={link.link} />
+                                    ))
                                 }
-                            }}
-                            onClick={() => {
-                                router.push('/akatsuki')
-                                handleClose()
-                            }}
-                        >
-                            Akatsuki</MenuItem>
-                        <MenuItem
-                            sx={{
-                                background: 'rgba(232, 107, 39, 0)',
-                                color: 'black',
-                                ":hover": {
-                                    background: 'rgba(232, 107, 39, 0)',
-                                    color: 'blue'
-                                }
-                            }}
-                            onClick={() => {
-                                router.push('/characters')
-                                handleClose()
-                            }}
-                        >Characters</MenuItem>
-                        <MenuItem
-                            sx={{
-                                background: 'rgba(232, 107, 39, 0)',
-                                color: 'black',
-                                ":hover": {
-                                    background: 'rgba(232, 107, 39, 0)',
-                                    color: 'blue'
-                                }
-                            }}
-                            onClick={() => {
-                                router.push('/clans')
-                                handleClose()
-                            }}
-                        >Clans</MenuItem>
-                        <MenuItem
-                            sx={{
-                                color: 'black',
-                                ":hover": {
-                                    color: 'blue'
-                                }
-                            }}
-                            onClick={() => {
-                                router.push('/kekkei-genkai')
-                                handleClose()
-                            }}
-                        >Kekkei Genkai</MenuItem>
-                        <MenuItem
-                            sx={{
-                                color: 'black',
-                                ":hover": {
-                                    color: 'blue'
-                                }
-                            }}
-                            onClick={() => {
-                                router.push('/tailed-beasts')
-                                handleClose()
-                            }}
-                        >Tailed Beasts</MenuItem>
-                        <MenuItem
-                            sx={{
-                                color: 'black',
-                                ":hover": {
-                                    color: 'blue'
-                                }
-                            }}
-                            onClick={() => {
-                                router.push('/teams')
-                                handleClose()
-                            }}
-                        >Teams</MenuItem>
-                        <MenuItem
-                            sx={{
-                                color: 'black',
-                                marginBottom: -1,
-                                ":hover": {
-                                    color: 'blue'
-                                }
-                            }}
-                            onClick={() => {
-                                router.push('/villages')
-                                handleClose()
-                            }}
-                        >Villages</MenuItem>
-                    </Menu>
-                </div>
-            </div>
-        </motion.nav >
+                            </motion.div>
+                        </div>
+                    </motion.div>
+                }
+            </AnimatePresence>
+        </header>
     )
 }
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Nova+Square&display=swap" rel="stylesheet" />
+      </head>
+
       <body
         className='gradient-bg text-white mx-[5vw] md:mx-[12vw] lg:mx-[15vw]'
       >
